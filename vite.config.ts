1536a928126f8f4b3d8c09511a25f9c7408da26f
Akhil Kumar <akhilnareshkumar@gmail.com>
2026-03-10 10:46:25 +0530

    Separate data and UI
    - The goal of this change is to make it easier to update the content of the website, without tweaking the UI.
    - Created /data folder to store all content.
    - Extracted the data from the HTML files and moved it to /data.
    - Updated the project to pick data from JSON and render it.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import handlebars from 'vite-plugin-handlebars';
 import { resolve } from 'node:path';
+import profileData from './data/profile.json';
+import metaData from './data/meta.json';
+import experienceData from './data/experience.json';
 
 import { cloudflare } from '@cloudflare/vite-plugin';
 
@@ -11,6 +14,11 @@ export default defineConfig({
     tailwindcss(),
     handlebars({
       partialDirectory: resolve(process.cwd(), 'src/components'),
+      context: {
+        profile: profileData,
+        meta: metaData,
+        ...experienceData, // spreading to easily pass `experiences` array
+      },
     }),
     cloudflare(),
   ],
