83a47006e06da9dc6a44e8daed9ca8c6b5459ef0
Akhil Kumar <akhilnareshkumar@gmail.com>
2026-04-09 10:42:18 +0530

    Add Astro to the project
    - After trying Astro in other website and seeing the benefits, setting it up here

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.astro'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
