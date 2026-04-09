43c3b0a38c230ab73f9a295ae012cff39b4590ec
Akhil Kumar <akhilnareshkumar@gmail.com>
2026-04-09 12:03:23 +0530

    Update ESLint config
    - Remove deprecated config method

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
 import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist', '.astro'] },
+export default defineConfig(
+  {
+    ignores: ['dist', '.astro'],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-    },
-    rules: {
-      // Add custom rules here
     },
   },
 );
